b34c31a258b3b07c2b540e5388a99701defc8128
Saronzeleke <Cassiopiasaron@gmail.com>
2025-05-12 01:31:50 -0700

    commit

--- main.py
+++ main.py
@@ -264,7 +264,7 @@ class KeystrokeCNN:
         model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         return model
 
-    def train(self, X_train, y_train, X_val, y_val, epochs=50, batch_size=32):
+    def train(self, X_train, y_train, X_val, y_val, epochs=70, batch_size=32):
         history = self.model.fit(
             X_train, y_train, validation_data=(X_val, y_val), epochs=epochs, batch_size=batch_size
         )
