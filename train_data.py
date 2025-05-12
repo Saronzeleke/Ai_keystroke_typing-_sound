35b73816c3ce712a778a0d5385f49554fe6986e1
Saronzeleke <Cassiopiasaron@gmail.com>
2025-05-12 00:37:41 -0700

    commit

--- train_data.py
+++ train_data.py
@@ -7,8 +7,6 @@ from sklearn.model_selection import train_test_split
 from scipy import signal
 import noisereduce as nr
 from tensorflow.keras.callbacks import ModelCheckpoint
-
-
 SAMPLE_RATE = 44100
 DURATION = 0.1
 N_MELS = 128
