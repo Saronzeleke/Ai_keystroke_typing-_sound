e7411f574bc795cb8cdaabf678a944c51ed69dce
Saronzeleke <Cassiopiasaron@gmail.com>
2025-05-19 22:54:46 -0700

    commit

--- train_data.py
+++ train_data.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import librosa
 from tensorflow.keras import layers, models
-from sklearn.model_selecetion  import train_test_split
+from sklearn.model_selecetion  import train_test_split 
 from scipy import signal
 import noisereduce as nr
 from tensorflow.keras.callbacks import ModelCheckpoint  
