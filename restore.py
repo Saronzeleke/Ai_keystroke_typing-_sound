2916eb5a8cea8bce6a1a40d41181f5343ab7e6cd
Saronzeleke <Cassiopiasaron@gmail.com>
2025-05-22 03:54:32 -0700

    commit

--- restore.py
+++ restore.py
@@ -1,30 +1,14 @@
 import os
 import shutil
-
 source_dir = 'validation_data'
 dest_dir = 'training_data'
-
-
-
-
-
-
-
-
-
-
 os.makedirs(dest_dir, exist_ok=True)
-
-
 for class_name in os.listdir(source_dir):
     class_path = os.path.join(source_dir, class_name)
     if not os.path.isdir(class_path):
         continue
-
     dest_class_path = os.path.join(dest_dir, class_name)
     os.makedirs(dest_class_path, exist_ok=True)
-
-   
     files = [f for f in os.listdir(class_path) if f.endswith('.wav')]
     for file_name in files:
         src = os.path.join(class_path, file_name)
